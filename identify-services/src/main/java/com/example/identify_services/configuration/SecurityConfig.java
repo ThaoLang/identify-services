b3caecc52528cc224338d7e7aed708fcdca1bc3c
ThaoLang <90896633+ThaoLang@users.noreply.github.com>
2025-02-19 09:44:15 +0700

    apply Spring Security and config to authenticate with jwt

---
+++ identify-services/src/main/java/com/example/identify_services/configuration/SecurityConfig.java
@@ -0,0 +1,60 @@
+package com.example.identify_services.configuration;
+
+import lombok.AccessLevel;
+import lombok.experimental.FieldDefaults;
+import lombok.experimental.NonFinal;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.oauth2.jose.jws.MacAlgorithm;
+import org.springframework.security.oauth2.jwt.JwtDecoder;
+import org.springframework.security.oauth2.jwt.NimbusJwtDecoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+import javax.crypto.spec.SecretKeySpec;
+
+@Configuration
+@EnableWebSecurity
+@FieldDefaults(level = AccessLevel.PRIVATE,makeFinal = true)
+public class SecurityConfig {
+    String[] PUBLIC_ENDPOINTS={
+            "/users",
+            "/auth/login",
+            "/auth/introspect"
+    };
+
+    @NonFinal
+    @Value("${jwt.secret}")
+    String secretKey;
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity httpSecurity) throws Exception {
+        httpSecurity.authorizeHttpRequests(request ->
+               request.requestMatchers(HttpMethod.POST,PUBLIC_ENDPOINTS).permitAll()
+                       .anyRequest().authenticated());
+
+        httpSecurity.csrf(AbstractHttpConfigurer::disable);
+
+        httpSecurity.oauth2ResourceServer(oauth2 ->
+                oauth2.jwt(jwtConfigurer -> jwtConfigurer.decoder(jwtDecoder()))
+        );
+
+        return httpSecurity.build();
+    }
+
+    @Bean
+    JwtDecoder jwtDecoder(){
+        SecretKeySpec secretKeySpec= new SecretKeySpec(secretKey.getBytes(),"HS512");
+
+        return NimbusJwtDecoder
+                .withSecretKey(secretKeySpec)
+                .macAlgorithm(MacAlgorithm.HS512)
+                .build();
+    }
+
+}
